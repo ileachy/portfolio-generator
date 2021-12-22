fe46ddf5f78dc82a457ac6218967e6dc7d36cdbc
Ian L <ileach81@gmail.com>
2021-12-21 21:50:51 -0500

    added template literals for HTML creation

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
+// collects command-line arguments
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+// info variables
+const [name, github] = profileDataArgs;
 
-const printProfileData = (profileDataArr) => {
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
+// Template literal objects
+const generatePage = (userName, githubName) => {
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
+
+  <body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
+  `;
 };
 
-printProfileData(profileDataArgs);
+console.log(name, github);
+console.log(generatePage(name, github));
