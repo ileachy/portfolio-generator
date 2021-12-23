fe2b482db91d51cc8e11a05646b022eecdc7eb8d
Ian L <ileach81@gmail.com>
2021-12-23 00:54:44 -0500

    daily commit before log-off

--- app.js
+++ app.js
@@ -1,15 +1,85 @@
-// allows fs to be used from node
-const fs = require("fs");
-// pulls the function from the other file
-const generatePage = require("./src/page-template.js");
-// slices out file data from outcome
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require("inquirer");
 
-const [name, github] = profileDataArgs;
+const promtUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username",
+    },
+    {
+      type: "input",
+      name: "about",
+      message: "Provide some information about yourself:",
+    },
+  ]);
+};
+
+const promptProject = () => {
+  console.log(`
+  `);
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the name of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Provide a description of the project (Required)",
+    },
+    {
+      type: "checkbox",
+      name: "languages",
+      message: "What did you build this project with? (Check all that apply)",
+      choices: [
+        "JavaScript",
+        "HTML",
+        "CSS",
+        "ES6",
+        "jQuery",
+        "Bootstrap",
+        "Node",
+      ],
+    },
+    {
+      type: "input",
+      name: "link",
+      message: "Enter the GitHub link to your project. (Required)",
+    },
+    {
+      type: "confirm",
+      name: "feature",
+      message: "Would you like to feature this project?",
+      default: false,
+    },
+    {
+      type: "confirm",
+      name: "confirmAddProject",
+      message: "Would you like to enter another project?",
+      default: false,
+    },
+  ]);
+};
+
+promtUser().then((answers) => console.log(answers));
+
+// allows fs to be used from node
+// const fs = require("fs");
+// pulls the function from the other file
+// const generatePage = require("./src/page-template.js");
+
+// const pageHTML = generatePage(name, github);
 
 // creates HTML file
-fs.writeFile("./index.html", generatePage(name, github), (err) => {
-  if (err) throw new Error(err);
+// fs.writeFile("./index.html", pageHTML, (err) => {
+// if (err) throw new Error(err);
 
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+// console.log("Portfolio complete! Check out index.html to see the output!");
+// });
